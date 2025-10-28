535e88044b7a0a70b1680bfc4ff628d73b71224b
DragonCat4012 <frida.shivaa@gmail.com>
2025-10-28 18:01:22 +0100

    init2

--- main.py
+++ main.py
@@ -15,7 +15,14 @@ def extract_zip(zip_file_path, extraction_folder):
 
     print(f"Successfully extracted to {extraction_folder}")
 
+    retunrstr = None
+    for item in os.listdir(extraction_folder):
+        if item.endswith(".xlsx"):
+            print(item)
+            retunrstr = os.path.join(extraction_folder, item)
     move_extracted_content(extraction_folder)
+
+    return retunrstr
 
 
 def move_extracted_content(parent_folder):
@@ -34,10 +41,12 @@ def move_extracted_content(parent_folder):
     nested_folder = os.path.join(parent_folder, extracted_dirs[0])
 
     print("---------------------")
+
     # Extract any ZIP files in the nested folder
     for item in os.listdir(nested_folder):
         print(item)
         item_path = os.path.join(nested_folder, item)
+
         if item.endswith(".zip"):
             extract_zip(item_path, nested_folder)
             # Move extracted contents up one level
@@ -47,9 +56,6 @@ def move_extracted_content(parent_folder):
 
                 if not "dropboxes" in extracted_item_path:
                     continue
-
-                if extracted_item.endswith(".xsl"):
-                    print(extracted_item)
 
                 print("\t" + extracted_item_path)
                 if os.path.isdir(extracted_item_path):
@@ -61,9 +67,10 @@ def move_extracted_content(parent_folder):
 
 
 def formatXSL(input_file_path):
-    output_file_path = "Bewertung.xls"  # Specify where to save the new .xls file
+    print("XSL: " + input_file_path)
 
-    # Specify the columns to remove (by name)
+    output_file_path = os.path.join(input_file_path.split("/")[0], "Bewertung.xlsx")
+
     columns_to_remove = [
         "Anrede",
         "Studiengruppe",
@@ -73,25 +80,20 @@ def formatXSL(input_file_path):
         "Studienabschluss",
         "Institution",
         "Standort",
-    ]  # Change these to the names of the columns to remove
+        "Startdatum",
+        "Dauer",
+    ]
 
-    # Call the function
     remove_columns_from_xls(input_file_path, output_file_path, columns_to_remove)
 
 
 def remove_columns_from_xls(input_file_path, output_file_path, columns_to_remove):
-    # Read the .xls file into a DataFrame
     df = pd.read_excel(input_file_path)
-
-    # Drop specified columns
     df.drop(columns=columns_to_remove, inplace=True)
-
-    # Save the modified DataFrame to a new .xls file
     df.to_excel(output_file_path, index=False)
 
 
 def extract_zipOLD(zip_file_path, extraction_folder):
-    # Create the extraction folder if it does not exist
     os.makedirs(extraction_folder, exist_ok=True)
 
     # Extract ZIP file
@@ -102,15 +104,16 @@ def extract_zipOLD(zip_file_path, extraction_folder):
 
 
 if __name__ == "__main__":
-    # Check for correct number of arguments
     if len(sys.argv) != 2:
         print("Usage: python script.py <zip_file_path> <extraction_folder>")
         sys.exit(1)
 
     # Get arguments
     zip_file_path = sys.argv[1]
-    # extraction_folder = sys.argv[2]
+    if os.path.exists("ZIP1"):
+        # Remove the directory and all its contents
+        shutil.rmtree("ZIP1")
+        print(f"Directory {'ZIP1'} has been deleted successfully.")
 
-    # Call the function to extract the ZIP file
     filePath = extract_zip(zip_file_path, "ZIP1")
     formatXSL(filePath)
